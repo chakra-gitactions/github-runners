d312952ea52f6e5577f0c0bd683e129f3c1ab83f
Chakradhar Jeereddy <79322615+Chakradhar-Jeereddy@users.noreply.github.com>
2026-01-28 01:14:50 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "chakra86.shop"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z01570372XTUEJNI70A9H"
+  description = "description"
+}
